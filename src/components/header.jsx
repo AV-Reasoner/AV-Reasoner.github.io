97cdf7e026ff10d790ed72c8da397a4871f75740
Lu Lidong <2421235683@qq.com>
2025-06-05 17:48:32 +0000

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -76,9 +76,9 @@ export default class Header extends React.Component {
           <div className="uk-container uk-container-small uk-section">
             <div className="uk-text-center uk-text-bold">
               <p className={titleClass}>{this.props.title}</p>
-              <span className="uk-label uk-label-primary uk-text-center uk-margin-small-bottom">
+              {/* <span className="uk-label uk-label-primary uk-text-center uk-margin-small-bottom">
                 {this.props.journal}
-              </span>
+              </span> */}
             </div>
             <Authors
               authors={this.props.authors}
